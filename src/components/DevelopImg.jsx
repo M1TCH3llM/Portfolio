22844940c36efcb4584a2b6a056f497ee7cb492e
Mitchell.Morgan <mitchemorgan@gmail.com>
2023-10-29 22:36:02 -0600

    added emailjs & links to projects

--- src/components/DevelopImg.jsx
+++ src/components/DevelopImg.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Frame from "../assets/PoliroidFrame.png";
 import Shred from "../assets/ShredNstyle.png";
 import Harry from "../assets/HPFP.png";
@@ -6,63 +6,78 @@ import Ecom from "../assets/ecomBackend.png";
 import Emp from "../assets/EmpLineCom.png";
 import Logo from "../assets/Logo.png";
 import Weather from "../assets/Weather.png";
-
 import "../styles/Develop.css";
 
 function DevelopImg() {
-    
-    const frame = document.getElementById("frame");
-  const fillImg = document.getElementById("fillImg");
+  useEffect(() => {
+    const imageSets = document.querySelectorAll(".developImgs");
 
-  for (let i = 0; i < frames.length; i++) {
-    frames[i].addEventListener("mouseenter", () => {
-      fillImgs[i].style.filter = "grayscale(0%) brightness(100%)";
-    });
+    imageSets.forEach((imageSet) => {
+      const fillImg = imageSet.querySelector(".fillImg");
 
-    frames[i].addEventListener("mouseleave", () => {
-      fillImgs[i].style.filter = "grayscale(100%) brightness(50%)";
+      imageSet.addEventListener("mouseenter", () => {
+        fillImg.style.filter = "grayscale(0%) brightness(100%)";
+      });
+
+      imageSet.addEventListener("mouseleave", () => {
+        fillImg.style.filter = "grayscale(100%) brightness(10%)";
+      });
     });
-  }
-    
-    
+  }, []);
+
   return (
     <>
-    <div style={{ border: "solid 2px red" }} className="developImgs">
-      <a>
-        <img id="fillImg" src={Shred} />
-        <img id="frame" src={Frame} />
-      </a>
-    </div>
-    <div style={{ border: "solid 2px red" }} className="developImgs">
-      <a>
-        <img id="fillImg" src={Harry} />
-        <img id="frame" src={Frame} />
-      </a>
-    </div>
-    <div style={{ border: "solid 2px red" }} className="developImgs">
-      <a>
-        <img id="fillImg" src={Ecom} />
-        <img id="frame" src={Frame} />
-      </a>
-    </div>
-    <div style={{ border: "solid 2px red" }} className="developImgs">
-      <a>
-        <img id="fillImg" src={Emp} />
-        <img id="frame" src={Frame} />
-      </a>
-    </div>
-    <div style={{ border: "solid 2px red" }} className="developImgs">
-      <a>
-        <img id="fillImg" src={Logo} />
-        <img id="frame" src={Frame} />
-      </a>
-    </div>
-    <div style={{ border: "solid 2px red" }} className="developImgs">
-      <a>
-        <img id="fillImg" src={Weather} />
-        <img id="frame" src={Frame} />
-      </a>
-    </div>
+      <div style={{ border: "solid 2px red" }} className="developImgs">
+        <a href="https://github.com/nlad218/ShredNStyle" target="_blank">
+          <h2>Shred'N Styles</h2>
+          <img className="fillImg" src={Shred} alt="Shred Image" />
+          <img className="frame" src={Frame} alt="Frame Image" />
+        </a>
+      </div>
+      <div style={{ border: "solid 2px red" }} className="developImgs">
+        <a
+          href="https://github.com/nfillip/harrypotter-fanpage"
+          target="_blank"
+        >
+          <h2>Fan Page</h2>
+          <img className="fillImg" src={Harry} alt="Harry Image" />
+          <img className="frame" src={Frame} alt="Frame Image" />
+        </a>
+      </div>
+      <div style={{ border: "solid 2px red" }} className="developImgs">
+        <a
+          href="https://github.com/M1TCH3llM/e_commerce_backend"
+          target="_blank"
+        >
+          <h2>Ecommerce Backend</h2>
+          <img className="fillImg" src={Ecom} alt="Ecom Image" />
+          <img className="frame" src={Frame} alt="Frame Image" />
+        </a>
+      </div>
+      <div style={{ border: "solid 2px red" }} className="developImgs">
+        <a href="https://github.com/M1TCH3llM/employee" target="_blank">
+          <h2>Employee Tracker</h2>
+          <img className="fillImg" src={Emp} alt="Emp Image" />
+          <img className="frame" src={Frame} alt="Frame Image" />
+        </a>
+      </div>
+      <div style={{ border: "solid 2px red" }} className="developImgs">
+        <a href="https://github.com/M1TCH3llM/LogoGen" target="_blank">
+          <h2>Logo Generator</h2>
+          <img className="fillImg" src={Logo} alt="Logo Image" />
+          <img className="frame" src={Frame} alt="Frame Image" />
+        </a>
+      </div>
+      <div style={{ border: "solid 2px red" }} className="developImgs">
+        <a
+          href="https://github.com/M1TCH3llM/TheBetterWeatherApp"
+          target="_blank"
+        >
+          <h2>Weather App</h2>
+          <img className="fillImg" src={Weather} alt="Weather Image" />
+          <img className="frame" src={Frame} alt="Frame Image" />
+        </a>
+      </div>
     </>
   );
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
-function Contact() {
+export const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_qcybaqv",
+        "template_j78zte8",
+        form.current,
+        "PbhdVPJMIXUqhGT-x"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
-    <div>Contact</div>
-  )
-}
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
 
-export default Contact
+export default Contact;
