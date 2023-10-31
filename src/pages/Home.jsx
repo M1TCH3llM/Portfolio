e35843878e790e2c9aee9963aab350d58f4fe6ea
Mitchell.Morgan <mitchemorgan@gmail.com>
2023-10-31 11:04:30 -0600

    final style changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
       </div>
       <div id="qBio">
         {" "}
-        Hi! I'm A Full stack developer with UX, UI design experience. I enjoy making easy and fun to use websites and apps. A lot of my inspiration for this portfolio came from the "outside art" movement. all img assets where home made by me for you. please take your time to look over my web site! and enjoy!!!
+        Hi! I'm A Full stack developer with UX, UI design experience. I enjoy making easy and fun to use websites and apps. all img assets where home made by me for you. please take your time to look over my web site! and enjoy!!!
       </div>
     </>
   );
