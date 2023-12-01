273a9a4f6872231e825e3e24ac8ae8828be52731
Mitchell.Morgan <mitchemorgan@gmail.com>
2023-12-01 12:04:15 -0700

    background change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,7 @@ function Home() {
             </div>
           </div>
           <div id="qBio">
-            Hi! I'm A Full Stack Developer with UX, UI Design experience. I
+            Hi! I'm a Full Stack Developer with UX, UI Design experience. I
             enjoy making easy and fun to use websites and apps. All img assets
             were home made by me for you. Please take your time to look over my
             web site! and enjoy!!!
