e11bba40fd8a698b390e8566be8b77dfaa0797de
Mitchell.Morgan <mitchemorgan@gmail.com>
2023-12-11 17:45:26 -0700

    updating CSS

--- src/components/Design/RetroTheater.jsx
+++ src/components/Design/RetroTheater.jsx
@@ -17,6 +17,7 @@ import NewMovie1 from "../../assets/UXAssets/NewMovie1.png";
 import NewMovie2 from "../../assets/UXAssets/NewMovie2.png";
 import CheckoutOld from "../../assets/UXAssets/CheckOutOld.png";
 import CheckoutNew from "../../assets/UXAssets/CheckoutNew.png";
+import "../../styles/NewRetro.css";
 import "../../styles/Retro.css";
 
 function RetroTheater() {
@@ -26,59 +27,61 @@ function RetroTheater() {
         <img id="retroBanner" src={RetroBanner} alt="retro movie banner" />
       </div>
       <div id="retroHeader">
-        <h1>Retro Theater</h1>
+        <h1 className="yellowFont">Retro Theater</h1>
       </div>
-      <div id="retroOverView">
+      <div className="yellowFont columnD">
         <h2>The Product</h2>
-        <div>
+        <p>
           The Retro Theater website was given to us by our client that ran a mom
           and pop movie theater that had heavy design influence from the 80's
           when they first opened. We wanted to create a website that brought the
           80's back to life and to personify the theaters vibe online.
-        </div>
+        </p>
       </div>
-      <div id="retroDuration">
-        <h2>Project Duration</h2>
-        <div>Duration February 1st 2023 to April 10th 2023</div>
+      <div className="yellowFont columnD">
+        <h3>Project Duration</h3>
+        <p>Duration February 1st 2023 to April 10th 2023</p>
       </div>
-      <div id="retroBreakDown">
+      <div className="yellowFont wrapCenter">
         <div className="retroBreakDown">
-          <h2>The Problem</h2>
-          <div>
+          <h3>The Problem</h3>
+          <p className="sPwidth">
             The theater we were working for had no online presence and it was
             difficult for movie goers to find showings and times, as well as
             book tickets in advance.
-          </div>
+          </p>
         </div>
         <div className="retroBreakDown">
-          <h2>The Goal</h2>
-          <div>
+          <h3>The Goal</h3>
+          <p className="sPwidth">
             The goal was to create a website that not only encapsulated the 80's
             retro theme of the theater, but also make it easy for users to
             navigate and book movie tickets online, while still conveying the
             message that this was a family owned and operated theater.
-          </div>
+          </p>
         </div>
         <div className="retroBreakDown">
-          <h2>My Role</h2>
-          <div>
+          <h3>My Role</h3>
+          <p className="sPwidth">
             My role throughout this project was lead designer, lead researcher,
             as well as taking on the role of graphic design for logos,
             background, and banners.
-          </div>
+          </p>
         </div>
         <div className="retroBreakDown">
-          <h2>Responsibilities</h2>
-          <div>
+          <h3>Responsibilities</h3>
+          <p className="sPwidth">
             My responsibilities for this project included but were not limited
             to, conducting research and compiling data, wireframe design, mockup
             design, graphic design, and prototype design.
-          </div>
+          </p>
         </div>
       </div>
       <div id="retroResearch">
-        <h2 id="retroResearchH2">Research Overview</h2>
-        <div>
+        <h2 className="pinkFont" id="retroResearchH2">
+          Research Overview
+        </h2>
+        <div className="yellowFont">
           <p>
             By conducting extensive user research and creating empathy maps, we
             were able to target pain points in the user experience. Our initial
@@ -88,8 +91,8 @@ function RetroTheater() {
             the movie, theater, and staff would make the experience more homely
             and welcoming. We aimed to create a unique experience distinct from
             other big box theaters and our research helped us achieve that goal.
-          </p>
-          <p>
+            <br />
+            <br />
             Throughout our research process, we gained valuable insights by
             conducting in-depth interviews with key users and developing user
             personas. Our focus was on catering to busy working individuals who
@@ -98,33 +101,33 @@ function RetroTheater() {
             introducing fresh perspectives and considerations. For instance,
             users emphasized the importance of theater amenities such as special
             events and food options.
-          </p>
-          <p>
+            <br />
+            <br />
             By incorporating these valuable insights, our application addresses
             all aspects of movie selection, while providing a comprehensive
             solution for my users.
           </p>
         </div>
       </div>
-      <div id="retroPain">
+      <div className="pinkFont" id="retroPain">
         <h2 id="retroPainH2">Pain Points</h2>
-        <div id="retroPainPoints">
+        <div id="retroPainPoints" className="wrapCenter">
           <div className="pain">
-            <h2>Navigation</h2>
+            <h3>Navigation</h3>
             <p>
               Users want a simple navigation experience that doesn't get them
               lost when looking for movies.
             </p>
           </div>
           <div className="pain">
-            <h2>Tickets</h2>
+            <h3>Tickets</h3>
             <p>
               Users want a simple navigation experience that doesn't get them
               lost when looking for movies.
             </p>
           </div>
           <div className="pain">
-            <h2>Movie Info</h2>
+            <h3>Movie Info</h3>
             <p>
               Users want a way to read about the movie to better understand if
               its a film they would like to see or not.
@@ -132,7 +135,7 @@ function RetroTheater() {
           </div>
         </div>
       </div>
-      <div id="retroPersonas">
+      <div className="pinkFont" id="retroPersonas">
         <h2>User Personas</h2>
         <div id="userContainer">
           <div className="retroUser">
@@ -145,28 +148,28 @@ function RetroTheater() {
           </div>
         </div>
       </div>
-      <div id="design">
+      <div className="blueFont" id="design">
         <h2>Design</h2>
       </div>
-      <div id="retroMap">
+      <div className="blueFont" id="retroMap">
         <img src={Map} alt="Retro Movie Map" />
         <div id="retroMapInfo">
           <h3>Site Map</h3>
-          <p>
+          <p className="yellowFont">
             My goal was to identify pain points that the user may have and
             design a site map that will help mitigate most pain points, as well
             as fulfill the users needs.
           </p>
         </div>
       </div>
-      <div id="paperWireFrame">
+      <div className="blueFont" id="paperWireFrame">
         <div>
           <h2>Paper Wire Frames</h2>
         </div>
         <div id="crazyEightsCon">
           <div>
             <h3>Crazy 8's</h3>
-            <p id="crazyP">
+            <p className="yellowFont" id="crazyP">
               This step was done with digital wireframes and only using crazy
               8's sketches as references. The reason for this was I already knew
               exactly where features were going to be incorporated into the
@@ -177,10 +180,10 @@ function RetroTheater() {
           <img src={Eights} alt="Crazy Eights Drawing" />
         </div>
       </div>
-      <div>
+      <div className="blueFont">
         <div id="paperInfo">
           <h2>Paper Wireframe Screen Size Variation</h2>
-          <p>
+          <p className="yellowFont">
             My goal during this process was to keep similar attributes and
             functionalities between screens. A user in theory would be able to
             navigate the phone screen just the same as the web page.
@@ -205,11 +208,11 @@ function RetroTheater() {
           </div>
         </div>
       </div>
-      <div>
+      <div className="blueFont">
         <div id="digitalWF">
           <h2>Digital WireFrames</h2>
           <div>
-            <p>
+            <p className="yellowFont">
               My goal was to make the site as easy to navigate as possible and
               display movies that would be most relevant to users, so searching
               for them would not be a hassle.
@@ -243,10 +246,10 @@ function RetroTheater() {
           </div>
         </div>
       </div>
-      <div id="lofiProtoCon">
+      <div className="blueFont" id="lofiProtoCon">
         <div id="lofiProto">
           <h2>Low Fidelity Prototype</h2>
-          <p>
+          <p className="yellowFont">
             Initially for my low fidelity prototype I wanted to simulate the
             user flow for booking a movie tickets. This way we can identify
             critical user issues and prototype shortcomings.
@@ -254,7 +257,7 @@ function RetroTheater() {
         </div>
         <img id="lofiProtoImg" src={LoFi} alt="" />
       </div>
-      <div id="userStudy">
+      <div className="yellowFont" id="userStudy">
         <h2>Usability Study</h2>
         <div>
           <h3>Parameters</h3>
@@ -342,8 +345,8 @@ function RetroTheater() {
               </p>
             </div>
             <div id="finding2Img">
-            <img src={NewMovie1} alt="New movie Wireframe" />
-            <img src={NewMovie2} alt="new booking wire frame" />
+              <img src={NewMovie1} alt="New movie Wireframe" />
+              <img src={NewMovie2} alt="new booking wire frame" />
             </div>
           </div>
           <div className="findingsDisplay">
@@ -357,6 +360,51 @@ function RetroTheater() {
             </div>
             <img src={CheckoutNew} alt="post user study checkout" />
           </div>
+        </div>
+      </div>
+      <div className="pinkFont">
+        <h2>Mockups</h2>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <img src="" alt="" />
+        <div>
+          <h3>Second User Study</h3>
+          <p className="yellowFont">
+            After a second usability study I found that the clashing of yellow
+            font on a blue background was difficult for users to see.
+          </p>
+        </div>
+        <img src="" alt="" />
+      </div>
+      <div className="pinkFont">
+        <h3>Mobile Mockups</h3>
+        <img src="" alt="" />
+      </div>
+      <div className="pinkFont">
+        <h3>High Fidelity Prototypes</h3>
+        <img src="" alt="" />
+      </div>
+      <a href="">Link To HiFi Prototypes</a>
+      <div className="pinkFont">
+        <h2>Final Thoughts</h2>
+        <div className="yellowFont">
+          <p>
+            During this project I really wanted to explore different design
+            techniques and color pallets as well as theme. It was difficult to
+            maintain an easy to read page with a vibrant color scheme and a
+            maximalist 80s design. However I feel that it was pulled off well
+            and meets the criteria for from and function.
+            <br />
+            <br />
+            I would like to finish off the other pages including but not limited
+            to snack ordering page, staff page, and about us page. as well as
+            continue testing after new features are added and reviewing old
+            feature to improve upon design.
+            <br />
+            <br />
+            Thank You For Reviewing This Project!
+          </p>
         </div>
       </div>
     </div>
